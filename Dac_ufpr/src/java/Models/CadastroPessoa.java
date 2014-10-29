bdbed00710997faf958892d828835efe6c4b3123
Samara Borges <samy.tiemi@gmail.com>
2014-10-29 15:53:29 -0200

    Teste de tudo

--- Dac_ufpr/src/java/Models/CadastroPessoa.java
+++ Dac_ufpr/src/java/Models/CadastroPessoa.java
@@ -1,8 +1,10 @@
 package Models;
 
 
+import Classes.Pessoa;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashSet;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,29 +15,15 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "CadastroPessoa", urlPatterns = {"/CadastroPessoa"})
 public class CadastroPessoa extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet CadastroPessoa</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet CadastroPessoa at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String action = request.getParameter("action");
+        
+        if (action.equals("inserirPessoa")){
+            Pessoa professor = new Pessoa();
+            professor.setNome(request.getParameter("nome"));
+            
+            
         }
     }
 
