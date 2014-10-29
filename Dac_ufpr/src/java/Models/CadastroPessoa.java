94753e5f6ce82a08306d0e9553cec5047b404de0
Samara Borges <samy.tiemi@gmail.com>
2014-10-29 15:18:27 -0200

    Criação do Pacote DAO e Models

--- Dac_ufpr/src/java/Models/CadastroPessoa.java
+++ Dac_ufpr/src/java/Models/CadastroPessoa.java
@@ -1,5 +1,5 @@
-
 package Models;
+
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,10 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Samara
- */
+
 @WebServlet(name = "CadastroPessoa", urlPatterns = {"/CadastroPessoa"})
 public class CadastroPessoa extends HttpServlet {
 
